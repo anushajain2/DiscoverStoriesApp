fe3bac3ae0b46d2051c02ccea504546f17f6d364
Anusha <anu@dhcp-hol-591.eduroam.cornell.edu>
2016-03-13 12:57:50 -0400

    Heroku config 4

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Made
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precomile = false
+    config.assets.paths << Rails.root.join('vendor', 'assets')
   end
 end
